76ebb9c7da47368fbe620a222d4e40bf99b5ea06
malgus-waf <129628996+malgus-waf@users.noreply.github.com>
2023-11-07 21:02:49 -0600

    Update 2-Subnets.tf
    corrected

--- 2-Subnets.tf
+++ 2-Subnets.tf
@@ -7,14 +7,41 @@ resource "aws_subnet" "public-eu-west-1a" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-eu-west-1a"
+    Name    = "public-eu-west-1a"
     Service = "application1"
-    Owner = "Chewbacca"
-    Planet = "Musafar"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+resource "aws_subnet" "public-eu-west-1b" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.32.2.0/24"
+  availability_zone       = "eu-west-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-eu-west-1b"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
   }
 }
 
 
+resource "aws_subnet" "public-eu-west-1c" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.32.3.0/24"
+  availability_zone       = "eu-west-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-eu-west-1c"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
 
 #these are for private
 resource "aws_subnet" "private-eu-west-1a" {
@@ -23,12 +50,36 @@ resource "aws_subnet" "private-eu-west-1a" {
   availability_zone = "eu-west-1a"
 
   tags = {
-    Name = "private-eu-west-1a"
+    Name    = "private-eu-west-1a"
     Service = "application1"
-    Owner = "Chewbacca"
-    Planet = "Musafar"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
+
+resource "aws_subnet" "private-eu-west-1b" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.32.12.0/24"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    Name    = "private-eu-west-1b"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
   }
 }
 
 
+resource "aws_subnet" "private-eu-west-1c" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.32.13.0/24"
+  availability_zone = "eu-west-1c"
 
+  tags = {
+    Name    = "private-eu-west-1c"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
+  }
+}
