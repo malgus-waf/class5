8afd24abca7c6be644c57addf42162cdf9ac67f6
malgus-waf <129628996+malgus-waf@users.noreply.github.com>
2023-11-07 20:22:13 -0600

    Create 9-LoadBalancer.tf
    pirate from Remo

---
+++ 9-LoadBalancer.tf
@@ -0,0 +1,30 @@
+resource "aws_lb" "app1_alb" {
+  name               = "app1-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.app1-sg02-LB01.id]
+  subnets            = [
+    aws_subnet.public-eu-west-1a.id,
+    aws_subnet.public-eu-west-1b.id,
+    aws_subnet.public-eu-west-1c.id
+  ]
+  enable_deletion_protection = true
+
+  tags = {
+    Name    = "App1LoadBalancer"
+    Service = "App1"
+    Owner   = "User"
+    Project = "Web Service"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app1_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app1_tg.arn
+  }
+}
