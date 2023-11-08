78239de923be65f58bc15b59d470a539bd56f93e
malgus-waf <129628996+malgus-waf@users.noreply.github.com>
2023-11-07 19:32:41 -0600

    Update 3-IGW.tf
    update from my computer

--- 3-IGW.tf
+++ 3-IGW.tf
@@ -1,7 +1,10 @@
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.app1.id
 
   tags = {
-    Name = "igw"
+    Name    = "app1_IG"
+    Service = "application1"
+    Owner   = "Luke"
+    Planet  = "Musafar"
   }
 }
