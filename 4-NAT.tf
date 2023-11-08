2ce8a7267c83d001b98e931c69fa2256ddde3a28
malgus-waf <129628996+malgus-waf@users.noreply.github.com>
2023-11-07 19:34:12 -0600

    Update 4-NAT.tf
    update from my computer

--- 4-NAT.tf
+++ 4-NAT.tf
@@ -8,7 +8,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public-us-east-1a.id
+  subnet_id     = aws_subnet.public-eu-west-1a.id
 
   tags = {
     Name = "nat"
