170f3c5709bef497342e0042532c21b9a02fec5f
malgus-waf <129628996+malgus-waf@users.noreply.github.com>
2023-11-11 15:19:56 -0600

    Update 11-Key.tf
    Testing Remo's change

--- 11-Key.tf
+++ 11-Key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "MyLinuxBox" {
   key_name   = "MyLinuxBox"
-  public_key = file("~/.ssh/mykey.pub")  # Path to your public key
+  public_key = file("${path.module}/MyLinuxBox.pem.pub")  # Path to your public key
 }
