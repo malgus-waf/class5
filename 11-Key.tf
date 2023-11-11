606b11cd3825db1f06c712824f7c3c8ffd366377
malgus-waf <129628996+malgus-waf@users.noreply.github.com>
2023-11-11 15:36:48 -0600

    Update 11-Key.tf
    Update from Motivated Mindset

--- 11-Key.tf
+++ 11-Key.tf
@@ -1,4 +1,17 @@
-resource "aws_key_pair" "MyLinuxBox" {
-  key_name   = "MyLinuxBox"
-  public_key = file("${path.module}/MyLinuxBox.pem.pub")  # Path to your public key
+resource "tls_private_key" "MyLinuxBox" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+data "tls_public_key" "MyLinuxBox" {
+  private_key_pem = tls_private_key.MyLinuxBox.private_key_pem
+}
+
+output "private_key" {
+  value     = tls_private_key.MyLinuxBox.private_key_pem
+  sensitive = true
+}
+
+output "public_key" {
+  value = data.tls_public_key.MyLinuxBox.public_key_openssh
 }
