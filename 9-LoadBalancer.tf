07922cadaf24acc29bb685424b1e229c9f6eb34d
malgus-waf <129628996+malgus-waf@users.noreply.github.com>
2023-11-18 15:21:57 -0600

    Update 9-LoadBalancer.tf
    443 update

--- 9-LoadBalancer.tf
+++ 9-LoadBalancer.tf
@@ -24,6 +24,26 @@ resource "aws_lb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
 
+data "aws_acm_certificate" "cert" {
+  domain   = "balekxnf;lknfglknd.com"
+  statuses = ["ISSUED"]
+  most_recent = true
+}
+
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app1_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"  # or whichever policy suits your requirements
+  certificate_arn   = data.aws_acm_certificate.cert.arn
+
+
+
+
+
+
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app1_tg.arn
