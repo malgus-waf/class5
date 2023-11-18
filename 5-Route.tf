94ac7ad9ecc852f9730cc1cdb6db56e5bafe2487
malgus-waf <129628996+malgus-waf@users.noreply.github.com>
2023-11-18 15:50:59 -0600

    Update 5-Route.tf
    routes

--- 5-Route.tf
+++ 5-Route.tf
@@ -59,6 +59,13 @@ resource "aws_route_table_association" "private-eu-west-1b" {
   subnet_id      = aws_subnet.private-eu-west-1b.id
   route_table_id = aws_route_table.private.id
 }
+resource "aws_route_table_association" "private-eu-west-1c" {
+  subnet_id      = aws_subnet.private-eu-west-1c.id
+  route_table_id = aws_route_table.private.id
+}
+
+
+#public
 
 resource "aws_route_table_association" "public-eu-west-1a" {
   subnet_id      = aws_subnet.public-eu-west-1a.id
@@ -67,5 +74,10 @@ resource "aws_route_table_association" "public-eu-west-1a" {
 
 resource "aws_route_table_association" "public-eu-west-1b" {
   subnet_id      = aws_subnet.public-eu-west-1b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-eu-west-1c" {
+  subnet_id      = aws_subnet.public-eu-west-1c.id
   route_table_id = aws_route_table.public.id
 }
